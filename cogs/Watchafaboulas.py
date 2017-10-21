7804a686716f6967883cb94ab8d4f3e6d42c11c5
Joseph <31323641+Polokniko@users.noreply.github.com>
2017-10-21 11:22:16 -0400

    Update Watchafaboulas.py

--- cogs/Watchafaboulas.py
+++ cogs/Watchafaboulas.py
@@ -1,13 +1,15 @@
 from discord.ext import commands
 
-
 class Watchafaboulas:
     def __init__(self, Human_Boi):
         self.Human_Boi = Human_Boi
 
-
-    @commands.command(name="ping")
+    @commands.command(name='ping',
+                      description="A command that will send the bot's latency.")
     async def ping(self, ctx):
-        await ctx.send("pong")
+        # ping
+        await ctx.send(f"{self.Human_Boi.latency * 1000} ms of delay.')
+
+
 def setup(Human_Boi):
     Human_Boi.add_cog(Watchafaboulas(Human_Boi))

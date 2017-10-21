b059801a0131e15c192e518bddc9a775ec05fae2
Joseph <31323641+Polokniko@users.noreply.github.com>
2017-10-21 11:16:56 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,26 +1,29 @@
 import discord
 from discord.ext import commands
 
-extensions = (
-    "cogs.Watchafaboulas"
+extension = (
+    'cogs.Watchafaboulas'
 )
-Human_Boi = commands.Bot(command_prefix="*")
 
+Human_Boi = commands.Bot(command_prefix='*')
 
 def main():
     @Human_Boi.event
     async def on_ready():
-        print("logged in as {},id = {}".format(Human_Boi.user.name, Human_Boi.user.name))
-        print("Users Connected {}.".format(str(len(set(Human_Boi.get_all_members())))))
-
+        print(f'\nLogged in as {Human_Boi.user.name} (ID: {Human_boi.user.id})')
+        print(f'Connected to {str(len(set(lambdabot.get_all_members())))} users')
+        print(f'Version: {discord.__version__}')
+        print('-' * 20)
+    
     for extension in extensions:
         try:
             Human_Boi.load_extension(extension)
         except Exception as e:
-            print('Failed to load extension: {}\n{}'.format(extension, e))
+            print(f'Failed to load extension: {extension}\n{e}')
         else:
-            print('Successfully loaded extension: ({})'.format(extension))
-        Human_Boi.run("""put token here""")
+            print(f'Successfully loaded extension: {extension}')
+
+        Human_Boi.run(token)
 
 
 if __name__ == '__main__':
